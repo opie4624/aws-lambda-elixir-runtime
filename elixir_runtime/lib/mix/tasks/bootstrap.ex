03bd5812c3f75619f612b58446a8a55e490b1004
Ryan C Koch <ryanckoch@gmail.com>
2019-09-04 01:55:43 -0600

    adding libtinfo.so.6

--- elixir_runtime/lib/mix/tasks/bootstrap.ex
+++ elixir_runtime/lib/mix/tasks/bootstrap.ex
@@ -40,7 +40,7 @@ defmodule Mix.Tasks.Bootstrap do
     \# So that distillery doesn't try to write any files
     export RELEASE_READ_ONLY=true
 
-    export LD_PRELOAD=$BASE/lib/#{@runtime_libs}/libcrypto.so.10
+    export LD_PRELOAD="$BASE/lib/#{@runtime_libs}/libcrypto.so.10 $BASE/lib/#{@runtime_libs}/libtinfo.so.6"
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BASE/lib/#{@runtime_libs}
 
     $EXE foreground
